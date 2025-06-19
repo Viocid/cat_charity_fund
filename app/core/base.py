e8d815ea7f5c040f9edf1afe37512a07d19ce19a
Viocid <versalione@gmail.com>
2025-06-19 08:57:33 +0300

    start_v2

--- app/core/base.py
+++
@@ -1,4 +0,0 @@
-# app/core/base.py
-"""Импорты класса Base и всех моделей для Alembic."""
-from app.core.db import Base  # noqa
-from app.models import  User  # noqa 
--- app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# app/models/__init__.py
-from .meeting_room import MeetingRoom
-from .reservation import Reservation
-from .user import User
