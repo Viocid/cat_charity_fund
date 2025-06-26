6c9d13a6b06348941bc753da0025d13302f63dd3
Viocid <versalione@gmail.com>
2025-06-26 12:23:48 +0300

    Tests_1

--- app/services/investments.py
+++ app/services/investments.py
@@ -10,6 +10,48 @@ from app.crud.donation import donation_crud
 from app.models import CharityProject, Donation
 
 
+def don_char(donations, projects, session):
+    donation_index = ZERO
+    project_index = ZERO
+    len_donations = len(donations)
+    len_projects = len(projects)
+    while (
+        donation_index < len_donations and
+        project_index < len_projects
+    ):
+        first_donation = donations[donation_index]
+        first_project = projects[project_index]
+        donation_capacity = (
+            first_donation.full_amount -
+            first_donation.invested_amount
+        )
+        project_capacity = (
+            first_project.full_amount -
+            first_project.invested_amount
+        )
+        min_capacity = min(donation_capacity, project_capacity)
+        first_donation.invested_amount += min_capacity
+        first_project.invested_amount += min_capacity
+
+        if (
+            first_donation.invested_amount ==
+            first_donation.full_amount
+        ):
+            first_donation.fully_invested = True
+            first_donation.close_date = datetime.now()
+            donation_index += ONE
+
+        if (
+            first_project.invested_amount ==
+            first_project.full_amount
+        ):
+            first_project.fully_invested = True
+            first_project.close_date = datetime.now()
+            project_index += ONE
+        session.add(first_donation)
+        session.add(first_project)
+
+
 async def investments(
     session: AsyncSession, new_db_obj: Union[CharityProject, Donation]
 ) -> Union[CharityProject, Donation]:
@@ -25,45 +67,7 @@ async def investments(
         )
         if not (donations or projects):
             return
-        donation_index = ZERO
-        project_index = ZERO
-        len_donations = len(donations)
-        len_projects = len(projects)
-        while (
-            donation_index < len_donations and
-            project_index < len_projects
-        ):
-            first_donation = donations[donation_index]
-            first_project = projects[project_index]
-            donation_capacity = (
-                first_donation.full_amount -
-                first_donation.invested_amount
-            )
-            project_capacity = (
-                first_project.full_amount -
-                first_project.invested_amount
-            )
-            min_capacity = min(donation_capacity, project_capacity)
-            first_donation.invested_amount += min_capacity
-            first_project.invested_amount += min_capacity
-
-            if (
-                first_donation.invested_amount ==
-                first_donation.full_amount
-            ):
-                first_donation.fully_invested = True
-                first_donation.close_date = datetime.now()
-                donation_index += ONE
-
-            if (
-                first_project.invested_amount ==
-                first_project.full_amount
-            ):
-                first_project.fully_invested = True
-                first_project.close_date = datetime.now()
-                project_index += ONE
-            session.add(first_donation)
-            session.add(first_project)
+        don_char(donations, projects, session)
 
         await session.commit()
         await session.refresh(new_db_obj)
